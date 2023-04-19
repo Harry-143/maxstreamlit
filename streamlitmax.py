c2205fa3f6a776c1e9529603c23d4555e6753a8d
Harry-143 <68056346+Harry-143@users.noreply.github.com>
2023-04-19 23:07:54 +0530

    Update streamlitmax.py

--- streamlitmax.py
+++ streamlitmax.py
@@ -2,21 +2,21 @@ import streamlit as st
 import pandas as pd
 
 st.header("""
-Largest number prediction App""")
+Max of three numbers """)
 
 st.subheader('User inputs')
-
+@st.cache_resource
 def user_input_features():
-  num1 = st.number_input("num1", min_value = -2000000000, max_value = 2000000000, step = 1)
-  num2 = st.number_input("num2", min_value = -2000000000, max_value = 2000000000, step = 1)
-  num3 = st.number_input("num3", min_value = -2000000000, max_value = 2000000000, step = 1)
+  num1 = st.number_input("Enter the first number", min_value = -999999999, max_value = 999999999, step = 1)
+  num2 = st.number_input("Enter the second number", min_value = -999999999, max_value = 999999999, step = 1)
+  num3 = st.number_input("Enter the third number", min_value = -999999999, max_value = 999999999, step = 1)
 
   numbers = [num1, num2, num3]
   return numbers
 numbers = user_input_features()
 res = max(numbers)
 
-
-if st.button('Find Lagrgest Number'):
+st.container("Voila!")
+if st.button('Find Largest Number'):
     st.write("Result : the largest of the 3 numbers is ")
     st.write(res)
