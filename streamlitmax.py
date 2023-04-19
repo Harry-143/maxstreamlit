6f51bdb44df4a948675da9c2dc68e3557f5960c3
Harry-143 <68056346+Harry-143@users.noreply.github.com>
2023-04-19 23:09:10 +0530

    Update streamlitmax.py

--- streamlitmax.py
+++ streamlitmax.py
@@ -5,7 +5,7 @@ st.header("""
 Max of three numbers """)
 
 st.subheader('User inputs')
-@st.cache_resource
+
 def user_input_features():
   num1 = st.number_input("Enter the first number", min_value = -999999999, max_value = 999999999, step = 1)
   num2 = st.number_input("Enter the second number", min_value = -999999999, max_value = 999999999, step = 1)
